e542812a89635ddee11a9ae0b654b2016b38de4c
Benjamn <bfreeman@lcs.on.ca>
2021-02-20 11:08:08 -0500

    Initial Commit

---
+++ UselessFacts/UselessFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UselessFactsApp.swift
+//  UselessFacts
+//
+//  Created by Ben Freeman on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct UselessFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
